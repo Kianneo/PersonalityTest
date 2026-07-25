fad50757f04ff91b34146d3923e142326acfae6b
REOSAPATRICIA <patriciareosa@gmail.com>
2026-07-25 11:15:22 +0800

    new

---
+++ Models/TestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PersonalityTest.Models
+{
+    public class TestViewModel
+    {
+        public List<Question> Questions { get; set; } = new();
+
+        public List<int> Answers { get; set; } = new();
+    }
+}
---
+++ Views/Test/Instructions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalityTest.Views.Test
+{
+    public class InstructionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
